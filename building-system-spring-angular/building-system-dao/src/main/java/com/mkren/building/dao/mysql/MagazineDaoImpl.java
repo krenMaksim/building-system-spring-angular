ecbd2a407e05746bb049196c9c78c7c8d09d87d0
krenMax <krenmv@belhard.com>
2018-07-21 08:14:25 +0300

    building-system: add new spring version

--- building-system-spring-angular/building-system-dao/src/main/java/com/mkren/building/dao/mysql/MagazineDaoImpl.java
+++ building-system-spring-angular/building-system-dao/src/main/java/com/mkren/building/dao/mysql/MagazineDaoImpl.java
@@ -10,44 +10,29 @@ import com.mkren.building.entity.MagazineEntity;
 
 @Repository("magazineDao")
 @Transactional(readOnly = false)
-
 public class MagazineDaoImpl extends BaseDao implements MagazineDAO {
-	@Override
-	public List<MagazineEntity> loadAllMagazine() {
-		String query = "SELECT mag FROM MagazineEntity mag";
+    @Override
+    public List<MagazineEntity> loadAllMagazine() {
+	String query = "SELECT mag FROM MagazineEntity mag";
 
-		return manager.createQuery(query, MagazineEntity.class).getResultList();
+	return manager.createQuery(query, MagazineEntity.class)
+	              .getResultList();
+    }
+
+    @Override
+    public MagazineEntity loadMagazineById(Integer magazineId) {
+	return manager.find(MagazineEntity.class, magazineId);
+    }
+
+    @Override
+    public MagazineEntity storeMagazine(MagazineEntity magazine) {
+	return manager.merge(magazine);
+    }
+
+    @Override
+    public void updateMagazine(MagazineEntity mag) {
+	if (mag.getId() != null) {
+	    manager.merge(mag);
 	}
-
-	@Override
-	public MagazineEntity loadMagazineById(Integer magazineId) {
-		return manager.find(MagazineEntity.class, magazineId);
-	}
-
-	@Override
-	public MagazineEntity storeMagazine(MagazineEntity magazine) {
-		return manager.merge(magazine);
-	}
-
-	@Override
-	public void updateMagazine(MagazineEntity mag) {
-		if (mag.getId() != null) {
-			manager.merge(mag);
-		}
-	}
-
-	// Query query = manager.createQuery("UPDATE MagazineEntity mag SET " + "mag.smeta = :smeta," + " mag.location = :location," + " mag.date = :date," + " mag.smena = :smena," + " mag.weather = :weather," + " mag.conditions = :conditions,"
-	// + " mag.volume = :volume," + " mag.controle = :controle," + " mag.user = :user WHERE mag.id = :id");
-	// query.setParameter("smeta", mag.getSmeta());
-	// query.setParameter("location", mag.getLocation());
-	// query.setParameter("date", mag.getDate());
-	// query.setParameter("smena", mag.getSmena());
-	// query.setParameter("weather", mag.getSmena());
-	// query.setParameter("conditions", mag.getConditions());
-	// query.setParameter("volume", mag.getVolume());
-	// query.setParameter("controle", mag.getControle());
-	// query.setParameter("smena", mag.getSmena());
-	// query.setParameter("user", mag.getUser());
-	// query.setParameter("id", mag.getId());
-	// query.executeUpdate();
+    }
 }
--- building-system-spring-angular/building-system-dao/src/main/java/com/mkren/building/spring/DaoConfig.java
+++ building-system-spring-angular/building-system-dao/src/main/java/com/mkren/building/spring/DaoConfig.java
@@ -1,76 +1,61 @@
 package com.mkren.building.spring;
 
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jndi.JndiTemplate;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 @Configuration
 @ComponentScan("com.mkren.building.dao.mysql")
 @EnableTransactionManagement
 public class DaoConfig {
-	// see /META-INF/persistence.xml
-	private static final String PERSISTENCE_UNIT_NAME = "building_system_db";
-	private static final String JNDI_RESOURCE_LINK = "java:comp/env/jdbc/building_system_resource";
+    private static final String PERSISTENCE_UNIT_NAME = "building_system_db";
+    @SuppressWarnings("unused")
+    private static final String JNDI_RESOURCE_LINK = "java:comp/env/jdbc/building_system_resource";
 
-	@Bean
-	DataSource dataSource() {
-		DataSource dataSource = null;
-		JndiTemplate jndi = new JndiTemplate();
-		try {
-			dataSource = jndi.lookup(JNDI_RESOURCE_LINK, DataSource.class);
-		} catch (NamingException e) {
-			throw new RuntimeException("NamingException for java:comp/env/jdbc/...", e);
-		}
-		return dataSource;
-	}
+    // @Bean
+    // DataSource dataSource() {
+    // DataSource dataSource = null;
+    // JndiTemplate jndi = new JndiTemplate();
+    // try {
+    // dataSource = jndi.lookup(JNDI_RESOURCE_LINK, DataSource.class);
+    // } catch (NamingException e) {
+    // throw new RuntimeException("NamingException for java:comp/env/jdbc/...", e);
+    // }
+    // return dataSource;
+    // }
 
-	@Bean(name = "entityManagerFactory")
-	public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
-		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-		emf.setDataSource(dataSource());
-		emf.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
-		// LoadTimeWeaver loadTimeWeaver = new InstrumentationLoadTimeWeaver();
-		// emf.setLoadTimeWeaver(loadTimeWeaver);
+    @Bean
+    public static DataSource dataSource() {
+	MysqlDataSource dataSource = new MysqlDataSource();
 
-		return emf;
-	}
+	// Set dataSource Properties
+	dataSource.setServerName("localhost");
+	dataSource.setPortNumber(3306);
+	dataSource.setDatabaseName("building_system");
+	dataSource.setUser("root");
+	dataSource.setPassword("root");
+	return dataSource;
+    }
 
-	@Bean
-	public JpaTransactionManager jpaTransMan() {
-		JpaTransactionManager jtManager = new JpaTransactionManager(getEntityManagerFactory().getObject());
-		return jtManager;
-	}
+    @Bean(name = "entityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactory() {
+	LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+	emf.setDataSource(dataSource());
+	emf.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
 
-	/*
-	 * @Bean(name = "entityManager") public EntityManager getEntityManager() { return getEntityManagerFactory().getObject().createEntityManager(); }
-	 */
+	return emf;
+    }
 
+    @Bean
+    public JpaTransactionManager jpaTransMan() {
+	JpaTransactionManager jtManager = new JpaTransactionManager(getEntityManagerFactory().getObject());
+	return jtManager;
+    }
 }
-
-// @Bean
-// public PersistenceAnnotationBeanPostProcessor persistenceAnnotationBeanPostProcessor() {
-// return new PersistenceAnnotationBeanPostProcessor();
-// }
-
-//// see /META-INF/persistence.xml
-// private static final String PERSISTENCE_UNIT_NAME = "building_system_db";
-//
-// @Bean
-// public JpaTransactionManager jpaTransMan() {
-// JpaTransactionManager jtManager = new JpaTransactionManager(getEntityManagerFactory().getObject());
-// return jtManager;
-// }
-//
-// @Bean
-// public LocalEntityManagerFactoryBean getEntityManagerFactory() {
-// LocalEntityManagerFactoryBean lemfb = new LocalEntityManagerFactoryBean();
-// lemfb.setPersistenceUnitName(PERSISTENCE_UNIT_NAME);
-// return lemfb;
-// }
