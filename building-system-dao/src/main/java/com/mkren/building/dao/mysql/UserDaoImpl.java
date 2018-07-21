2396c0ae595fc0bbc6b396d6fa6dc6507494556b
krenMax <krenmv@belhard.com>
2018-07-21 13:12:54 +0300

    add test for UserDaoImpl.java

--- building-system-dao/src/main/java/com/mkren/building/dao/mysql/UserDaoImpl.java
+++ building-system-dao/src/main/java/com/mkren/building/dao/mysql/UserDaoImpl.java
@@ -1,6 +1,7 @@
 package com.mkren.building.dao.mysql;
 
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Query;
 
@@ -12,31 +13,36 @@ import com.mkren.building.entity.UserEntity;
 @Repository("userDao")
 public class UserDaoImpl extends BaseDao implements UserDAO {
 
-	@Override
-	public List<UserEntity> loadAllUsers() {
-		String query = "SELECT u FROM UserEntity u";
+    @Override
+    public List<UserEntity> loadAllUsers() {
+	String query = "SELECT u FROM UserEntity u";
 
-		return manager.createQuery(query, UserEntity.class).getResultList();
-	}
+	return manager.createQuery(query, UserEntity.class)
+	              .getResultList();
+    }
 
-	@Override
-	public UserEntity loadUserById(Integer userId) {
-		return manager.find(UserEntity.class, userId);
-	}
+    @Override
+    public UserEntity loadUserById(Integer userId) {
+	Objects.requireNonNull(userId);
 
-	@Override
-	public UserEntity loadUserByLogin(String login) {
-		Query query = manager.createQuery("SELECT u FROM UserEntity u WHERE u.login = :login");
-		query.setParameter("login", login);
+	return manager.find(UserEntity.class, userId);
+    }
 
-		return (UserEntity) query.getSingleResult();
-	}
+    @Override
+    public UserEntity loadUserByLogin(String login) {
+	Objects.requireNonNull(login);
 
-	@Override
-	public UserEntity loadUserBySurname(String surname) {
-		Query query = manager.createQuery("SELECT u FROM UserEntity u WHERE u.surnameInitials = :surname");
-		query.setParameter("surname", surname);
+	Query query = manager.createQuery("SELECT u FROM UserEntity u WHERE u.login = :login");
+	query.setParameter("login", login);
 
-		return (UserEntity) query.getSingleResult();
-	}
+	return (UserEntity) query.getSingleResult();
+    }
+
+    @Override
+    public UserEntity loadUserBySurname(String surname) {
+	Query query = manager.createQuery("SELECT u FROM UserEntity u WHERE u.surnameInitials = :surname");
+	query.setParameter("surname", surname);
+
+	return (UserEntity) query.getSingleResult();
+    }
 }
--- building-system-dao/src/test/java/com/mkren/building/dao/mysql/UserDaoImplTest.java
+++ building-system-dao/src/test/java/com/mkren/building/dao/mysql/UserDaoImplTest.java
@@ -1,25 +1,75 @@
 package com.mkren.building.dao.mysql;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Objects;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mkren.building.dao.UserDAO;
+import com.mkren.building.entity.UserEntity;
 
 class UserDaoImplTest extends AbstractDaoTest {
+
+    private static final UserEntity expectedUser = new UserEntity();
+    static {
+	expectedUser.setId(1);
+	expectedUser.setLogin("Petrov");
+	expectedUser.setPassword("dfssf35");
+	expectedUser.setSurnameInitials("Петров И.И.");
+	expectedUser.setRole("заказчик");
+	expectedUser.setDelStatus("активен");
+    }
 
     @Autowired
     private UserDAO userDao;
 
     @Test
-    void test() {
-	userDao.loadAllUsers()
-	       .forEach(i -> System.out.println(i));
+    void loadAllUsers() {
+	List<UserEntity> users = userDao.loadAllUsers();
+
+	assertFalse(users.isEmpty());
+	assertEquals(4, users.size());
+
+	users.forEach(user -> assertTrue(Objects.nonNull(user)));
     }
 
     @Test
-    void test2() {
-	userDao.loadAllUsers()
-	       .forEach(i -> System.out.println(i));
+    void loadUserById() {
+	assertEquals(expectedUser, userDao.loadUserById(expectedUser.getId()));
     }
 
+    @Test
+    void loadUserByFakeId() {
+	assertEquals(null, userDao.loadUserById(-1));
+    }
+
+    @Test
+    void loadUserByNullId() {
+	assertThrows(NullPointerException.class, () -> {
+	    userDao.loadUserById(null);
+	});
+    }
+
+    @Test
+    void loadUserByLogin() {
+	assertEquals(expectedUser, userDao.loadUserByLogin(expectedUser.getLogin()));
+    }
+
+    @Test
+    void loadUserByFakeLogin() {
+	assertEquals(null, userDao.loadUserByLogin("no user"));
+    }
+
+    @Test
+    void loadUserByNullLogin() {
+	assertThrows(NullPointerException.class, () -> {
+	    userDao.loadUserByLogin(null);
+	});
+    }
 }
